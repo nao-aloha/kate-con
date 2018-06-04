6fb60a3d00b0d552ff08c0078dabae2180706887
Nao <passmethepoke@gmail.com>
2018-06-04 18:40:46 +0900

    generate取り消し実行　bin/rails destroy migration AddHourly_rateToCompanies hourly_rate: integer

--- db/migrate/20180604073014_add_hourly_rate_to_companies.rb
+++
@@ -1,6 +0,0 @@
-class AddHourlyRateToCompanies < ActiveRecord::Migration[5.2]
-  def change
-    add_column :companies, :hourly_rate, :string
-    add_column :companies, :integer, :string
-  end
-end
