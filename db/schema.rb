3d1b42d20a483ee914f2843213eca1c3460b60e1
Nao <passmethepoke@gmail.com>
2018-06-05 20:50:43 +0900

    migration実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_04_094235) do
+ActiveRecord::Schema.define(version: 2018_06_05_114811) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -48,6 +48,14 @@ ActiveRecord::Schema.define(version: 2018_06_04_094235) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "hourly_rate"
+  end
+
+  create_table "courses", force: :cascade do |t|
+    t.integer "company_id"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["company_id"], name: "index_courses_on_company_id"
   end
 
 end
