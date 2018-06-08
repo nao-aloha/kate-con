8f41c2324a09da4d0fe4b0ff555ed09bd4b3d75b
Nao <passmethepoke@gmail.com>
2018-06-08 18:21:01 +0900

    会社登録画面に授業目的登録機能の追加

--- app/admin/companies.rb
+++ app/admin/companies.rb
@@ -1,6 +1,29 @@
 ActiveAdmin.register Company do
 
-	permit_params :name, :hourly_rate, :lesson_purpose_id, :lesson_purpose_name	
+	permit_params :name, :hourly_rate, :lesson_purpose_id, :lesson_purpose_name,  company_lesson_purposes_attributes: [:id, :lesson_purpose, :company_lesson_purposes_id, :_destroy]	
+
+	form(:html => { :multipart => true }) do |f|
+    f.inputs "Details" do
+      f.input :name
+      f.input :hourly_rate
+      #f.input :lesson_purpose_name
+      
+    end
+    
+    f.inputs "lesson_purposes" do
+      f.has_many :company_lesson_purposes do |t|
+        t.input :lesson_purpose
+      end
+    end
+    #f.inputs do
+      #f.has_many :lesson_purposes, heading: 'LessonPurpose', allow_destroy: true, new_record: true do |a|
+        #a.input :lesson_purpose_text
+        #a.input :total_count
+      #end
+    #end
+ 
+    f.actions
+  end
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
