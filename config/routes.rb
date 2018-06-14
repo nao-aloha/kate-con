b5c28fd08616773d4b5fa040ee42e21049007554
Nao <passmethepoke@gmail.com>
2018-06-14 17:22:24 +0900

    lesson情報をindexとshowページに表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'lessons/index'
-  get 'lessons/show'
+  
 	resources :companies, :only => [:index, :show]
+	resources :lessons, :only => [:index, :show]
 	devise_for :admin_users, ActiveAdmin::Devise.config
 	ActiveAdmin.routes(self)
 end
