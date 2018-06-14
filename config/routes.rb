d39fc8c7b09da8169d004092c78c2b33496412e9
Nao <passmethepoke@gmail.com>
2018-06-14 17:08:21 +0900

    lessons情報の表示ページ作成 bin/rails g controller Lessons index show実行

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'lessons/index'
+  get 'lessons/show'
 	resources :companies, :only => [:index, :show]
 	devise_for :admin_users, ActiveAdmin::Devise.config
 	ActiveAdmin.routes(self)
