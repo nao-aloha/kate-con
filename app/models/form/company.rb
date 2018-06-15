135be61254c448415a313a2476d0032b7ef7415a
Nao <passmethepoke@gmail.com>
2018-06-15 22:58:08 +0900

    company_lesson_purpose紐付け解除２

--- app/models/form/company.rb
+++ app/models/form/company.rb
@@ -1,5 +1,5 @@
 class Form::Company < Company
   REGISTRABLE_ATTRIBUTES = [:id, :name]
   has_many :lessons
-  has_many :company_lesson_purposes, class_name: 'Form::CompanyLessonPurpose'
+  #has_many :company_lesson_purposes, class_name: 'Form::CompanyLessonPurpose'
 end
