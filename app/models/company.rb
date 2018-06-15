310582d4d139886e57a1884013b43f5189b6722b
Nao <passmethepoke@gmail.com>
2018-06-15 19:19:49 +0900

    company_lesson_purposesテーブルとの紐付け解除

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,8 +1,8 @@
 class Company < ApplicationRecord
 	
-  has_many :lesson_purposes, through: :company_lesson_purposes
-  has_many :company_lesson_purposes
+  # has_many :lesson_purposes, through: :company_lesson_purposes
+  # has_many :company_lesson_purposes
   has_many :lessons
-  accepts_nested_attributes_for :company_lesson_purposes 
+  #accepts_nested_attributes_for :company_lesson_purposes 
   accepts_nested_attributes_for :lessons
 end
--- app/models/lesson_purpose.rb
+++ app/models/lesson_purpose.rb
@@ -1,7 +1,7 @@
 class LessonPurpose < ApplicationRecord
   	
 	
-  has_many :companies, through: :company_lesson_purposes
-  has_many :company_lesson_purposes
+  # has_many :companies, through: :company_lesson_purposes
+  # has_many :company_lesson_purposes
   
 end
