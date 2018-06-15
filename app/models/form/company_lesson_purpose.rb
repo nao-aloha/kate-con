d72a95e2bc8dd0e92bf8da35b2e2b0bb59dafb18
Nao <passmethepoke@gmail.com>
2018-06-15 19:03:39 +0900

    company_lesson_purposesモデル、関連ファイルの削除

--- app/models/form/company_lesson_purpose.rb
+++
@@ -1,7 +0,0 @@
-class Form::CompanyLessonPurpose < CompanyLessonPurpose
-  REGISTRABLE_ATTRIBUTES = [:id, :company_id, :lesson_purpose_id, :_destroy]
-
-  def selectable_lesson_purposes
-    LessonPurpose.all
-  end
-end
--- test/models/company_lesson_purpose_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class CompanyLessonPurposeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
