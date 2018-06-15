9a6d04eb8d61a4ee5117359e572353e1127d5d87
Nao <passmethepoke@gmail.com>
2018-06-15 23:49:52 +0900

    中間テーブルlesson_purposesテーブルの作成

---
+++ app/models/lesson_purpose.rb
@@ -0,0 +1,4 @@
+class LessonPurpose < ApplicationRecord
+  belongs_to :lesson
+  belongs_to :purpose
+end
---
+++ test/models/lesson_purpose_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class LessonPurposeTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
